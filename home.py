67b2325548c7f48977a0d661ce573f32a3840eb8
DILEESH-PV <dileeshpv1998@gmail.com>
2022-11-07 10:03:54 +0530

    operation

--- home.py
+++ home.py
@@ -5,6 +5,8 @@ try:
     mydb=mysql.connector.connect(host='localhost',user='root',password='',database='homeautomationdb')
 except mysql.connector.Error as e:
     sys.exit("db connection error")
+mycursor=mydb.cursor()
+    
 
 while True:
     print("\nSelect an option")
@@ -13,12 +15,42 @@ while True:
     print("3. Search data by date")
     print("4. Exit")
 
-    choice = int(input("Enter an option: "))
-    if(choice == 1):
-        print("Enter the values\n")
-    elif(choice ==2):
-        print("View all values\n")
-    elif(choice == 3):
-        print("Search a value\n")
-    elif(choice==4):
+    ch = int(input("Enter an option: "))
+    
+    if (ch==1):
+        temp=input("Enter the temperature")
+        hum=input("Enter the humidity")
+        moi=input("Enter the moisture")
+        date=datetime.today().strftime('%Y-%m-%d')
+        try:
+            sql="INSERT INTO `sensorvalues`(`temperature`, `humidity`, `moisture`, `date`)  VALUES (%s,%s,%s,now())"
+            data=(temp,hum,moi)
+            mycursor.execute(sql,data)
+            mydb.commit()        
+            print("inserted success")
+        except mysql.connector.Error as e:
+            sys.exit("db connection error")
+    elif(ch ==2):
+        try:
+            sql="SELECT `temperature`, `humidity`, `moisture`,`date` FROM `sensorvalues`"
+            mycursor.execute(sql)
+            result=mycursor.fetchall()
+        except mysql.connector.Error as e:
+            sys.exit("db connection error")
+        for i in result:
+            print(i)
+        print("Displayed all data")
+        
+    elif(ch == 3):
+        date = input("Enter the date: ")
+        sql = "SELECT `temperature`, `humidity`, `moisture` FROM `sensorvalues` WHERE `date`= '"+date+"'"
+        mycursor.execute(sql)
+        result = mycursor.fetchall()
+        print(result)
+        mycursor.execute(sql)
+        result=mycursor.fetchall()
+        for i in result:
+            print(i)
+        print("Displayed all data")
+    elif(ch==4):
         break
